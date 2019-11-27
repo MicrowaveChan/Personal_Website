dbe3b25d0b20613fe0f3f18d29b156a0c56700da
Robert <robert.cn.michaud@hotmail.com>
2019-11-26 21:54:10 -0500

    minor changes

--- app.py
+++ app.py
@@ -4,24 +4,25 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', active_page='projects')
 
 
 @app.route('/art')
 def art():
-    return render_template('art.html')
+    return render_template('art.html', active_page='art')
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', active_page='contact')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
