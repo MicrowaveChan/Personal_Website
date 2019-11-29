97f3a649ce9102586dec5a6cd4fe8a7875bddc43
Robert <robert.cn.michaud@hotmail.com>
2019-11-28 20:12:00 -0500

    started implementing forms

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
+import csv
 from flask import Flask, render_template
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '4f3c2b1a6c977eef'
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    with open('static/data/skills.csv') as f:
+        skill_list = list(csv.reader(f))[1:]
+    return render_template('home.html', skill_list=skill_list)
 
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html', active_page='projects')
+    with open('static/data/projects.csv') as f:
+        project_list = list(csv.reader(f))[1:]
+    return render_template('projects.html', active_page='projects',
+                            project_list=project_list)
 
 
 @app.route('/art')
@@ -21,7 +29,20 @@ def art():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', active_page='contact')
+    form = ContactForm()
+    return render_template('contact.html', active_page='contact', form=form)
+
+
+@app.route('/register')
+def register():
+    form = SignupForm()
+    return render_template('register.html', active_page='register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, Regexp
+
+class SignupForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    username = StringField('Username', validators=[InputRequired(),
+                            Length(min=3, max=15),
+                            Regexp('^[A-Za-z][A-Za-z0-9_]*$', 0,
+                            'Username must start with a letter and can only contain letters, numbers and underscores.')])
+    password = PasswordField('Password', validators=[InputRequired(),
+                            Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(),
+                            EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(),
+                            Length(min=6, max=20)])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Login')
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    message = StringField('Message', validators=[InputRequired()])
+    submit = SubmitField('Login')
