4f61af945bc7f652e92e4ba6133725f28d852975
Robert Michaud <robert.cn.michaud@hotmail.com>
2019-12-21 07:46:08 +0000

    config.py set to load from server config and changes to templates

--- personal_website/config.py
+++ personal_website/config.py
@@ -1,5 +1,10 @@
-import os
+# import os
+import json
+# with open for config_file
+with open('/etc/flask-config.json') as config_file:
+    config = json.load(config_file)
 
+# os.environ.get for environment variables
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
